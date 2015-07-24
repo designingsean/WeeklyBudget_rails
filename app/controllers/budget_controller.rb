be3d1b92c8a683aa98f1645f8a1b3d7c0fc4ee9e
Sean Ryan <sean@designingsean.com>
2015-07-24 11:27:00 -0400

    Add monthly and weekly current totals

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -1,4 +1,6 @@
 class BudgetController < ApplicationController
   def index
+    @monthly_sum = MONTHLY_BUDGET - Expense.monthly_sum
+    @weekly_sum = WEEKLY_BUDGET - Expense.weekly_sum
   end
 end
