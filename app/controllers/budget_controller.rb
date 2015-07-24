2af05004040da2042b27df83a3248c84b9f9f046
Sean Ryan <sean@designingsean.com>
2015-07-24 11:27:28 -0400

    Add logic for progress bar display

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -2,5 +2,6 @@ class BudgetController < ApplicationController
   def index
     @monthly_sum = MONTHLY_BUDGET - Expense.monthly_sum
     @weekly_sum = WEEKLY_BUDGET - Expense.weekly_sum
+    @weekly_percentage = (@weekly_sum/WEEKLY_BUDGET)*100
   end
 end
