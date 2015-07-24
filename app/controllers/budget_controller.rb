f9bbe84e240410b842049dfa208bcf8d4b15f3b6
Sean Ryan <sean@designingsean.com>
2015-07-24 11:28:37 -0400

    Add route and logic to add expense

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -4,5 +4,17 @@ class BudgetController < ApplicationController
     @weekly_sum = WEEKLY_BUDGET - Expense.weekly_sum
     @weekly_percentage = (@weekly_sum/WEEKLY_BUDGET)*100
     @expenses = Expense.all.reverse
+    @new_expense = Expense.new
+  end
+
+  def create
+    Expense.create(expense_params)
+    redirect_to controller: "budget", action: "index"
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:amount, :postDate)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'budget/index'
+  post 'budget/create'
 
   root 'budget#index'
 end
