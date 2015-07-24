ec729d4dc95135da8226dc6db6ca2f3a2df2bdd4
Sean Ryan <sean@designingsean.com>
2015-07-24 11:25:21 -0400

    Add methods for monthly and weekly sums to model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,8 @@
 class Expense < ActiveRecord::Base
+  def self.monthly_sum
+    self.where("Month(postDate) = ?", Date.today.month).sum(:amount)
+  end
+  def self.weekly_sum
+    self.where("WEEK(postDate, 1) = ?", Date.today.cweek).sum(:amount)
+  end
 end
