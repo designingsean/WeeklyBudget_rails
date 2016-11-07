bf3b0ac22fa1d5e05c4198d11bec115781e40b33
Sean Ryan <sean@designingsean.com>
2016-11-07 08:57:44 -0500

    Remove weekly number and update statement break date

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,15 @@
 class Expense < ActiveRecord::Base
   def self.monthly_sum
-    self.where("Month(postDate) = ?", Date.today.month).sum(:amount)
+    statement_break = Date.new(Date.today.year, Date.today.month, 20)
+    if Date.today <= statement_break
+      start_date = statement_break + 1.day - 1.month
+      end_date = statement_break
+    else
+      start_date = statement_break + 1.day
+      end_date = statement_break + 1.month
+    end
+    self.where(:postDate => start_date..end_date).sum(:amount)
+    # self.where("Month(postDate) = ?", Date.today.month).sum(:amount)
   end
   def self.weekly_sum
     self.where("WEEK(postDate, 1) = ?", Date.today.cweek).sum(:amount)
