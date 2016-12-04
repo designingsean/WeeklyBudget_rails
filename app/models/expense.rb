42515c70dc3f5e7889bb4e752f654badf01fcfa1
Sean Ryan <sean@designingsean.com>
2016-12-03 23:00:47 -0500

    Fix a couple of bugs

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,20 +1,20 @@
 class Expense < ActiveRecord::Base
+  statement_break = Date.new(Date.today.year, Date.today.month, 20)
+  if Date.today <= statement_break
+    start_date = statement_break + 1.day - 1.month
+    end_date = statement_break
+  else
+    start_date = statement_break + 1.day
+    end_date = statement_break + 1.month
+  end
+
+  scope :monthly, -> { where(:postDate => start_date..end_date) }
+
   def self.monthly_sum
-    statement_break = Date.new(Date.today.year, Date.today.month, 20)
-    if Date.today <= statement_break
-      start_date = statement_break + 1.day - 1.month
-      end_date = statement_break
-    else
-      start_date = statement_break + 1.day
-      end_date = statement_break + 1.month
-    end
-    self.where(:postDate => start_date..end_date).sum(:amount)
-    # self.where("Month(postDate) = ?", Date.today.month).sum(:amount)
+    self.monthly.sum(:amount)
   end
-  def self.weekly_sum
-    self.where("WEEK(postDate, 1) = ?", Date.today.cweek).sum(:amount)
-  end
+
   def self.monthly_items
-    self.where("Month(postDate) = ?", Date.today.month).reverse
+    self.monthly.reverse
   end
 end
